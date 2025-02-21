b42978adad4d84d8b0a98d09946b52a0d8f560a8
Jiwon Choi <jiwon94k@gmail.com>
2025-02-22 02:20:11 +0900

    Feat: [Header] 컴포넌트 생성
    - 상수(색상) 추가
    - 아이콘(말풍선, 아래화살표, 서류상자) 추가

---
+++ src/constants/colors.ts
@@ -0,0 +1,11 @@
+export const COLORS = {
+    GR_20: '#F8F9FC',
+    GR_50: '#8D8F95',
+    GR_90: '#282929',
+    GR_BORDER: '#E3E5EB',
+    GR_DARK: '#434344',
+
+    ORANGE: '#EF5C00',
+    ORANGE_MD: '#FFF1DF',
+    ORANGE_LIGHT: '#FFF9F2',
+} as const;
