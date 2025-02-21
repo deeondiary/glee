d61c546d78caf0676b4f5a528acef960b60d3b09
Jiwon Choi <jiwon94k@gmail.com>
2025-02-22 02:25:27 +0900

    Feat: [공통] 레이아웃 지정
    - 새로고침 시 ui 깨짐 이슈 수정
    - 모바일웹 ui 레이아웃 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import Header from "@/src/components/Header";
+import StyledComponentsRegistry from "@/src/lib/registry";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,9 +15,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
+        <body>
+        <StyledComponentsRegistry >
+              <div className="wrapper">
+                <Header />
+                <div>
+                  {children}
+                </div>
+              </div>
+        </StyledComponentsRegistry>
+        </body>
     </html>
   );
 }
