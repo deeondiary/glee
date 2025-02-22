5a3737696435a20eba736bc747ce9288218cd75d
Jiwon Choi <jiwon94k@gmail.com>
2025-02-22 23:52:12 +0900

    Fix: styled-component prop 추가 에러 수정

--- src/components/Chip.tsx
+++ src/components/Chip.tsx
@@ -3,15 +3,16 @@ import React, {useState} from 'react';
 import styled from "styled-components";
 import {COLORS} from "@/src/constants/colors";
 
+
 interface ChipProps {
     children: string;
     onClick?: () => void | undefined;
     active?: boolean;
 }
+
 function Chip(props: ChipProps) {
     const [isActive, setIsActive] = useState<boolean>(false);
     const handleClick = () => {
-        console.log('no ??', isActive)
         setIsActive(!isActive);
         props.onClick();
     }
@@ -21,14 +22,19 @@ function Chip(props: ChipProps) {
         </ChipContainer>
     );
 }
-const ChipContainer = styled.div<{active: string}>`
-    background-color: ${(p) => p.active === 'active' ? COLORS.ORANGE_LIGHT : COLORS.GR_20};
+
+interface ChipContainerProps {
+    $active?: boolean
+}
+
+const ChipContainer = styled.div<ChipContainerProps>`
+    background-color: ${(p) => p.$active ? COLORS.ORANGE_LIGHT : COLORS.GR_20};
     cursor: pointer;
     padding: 20px;
     border-radius: 34px;
     border: 1px solid ${COLORS.GR_20};
     display: inline-block;
-    border-color: ${(p) => p.active === 'active' ? COLORS.ORANGE : COLORS.GR_20};
+    border-color: ${(p) => p.$active ? COLORS.ORANGE : COLORS.GR_20};
 `
 const ChipText = styled.span`
     font-weight: 600;
