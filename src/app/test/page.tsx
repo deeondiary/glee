5e8071d7873fb613dac3077ade2a81e668be5ce8
Jiwon Choi <jiwon94k@gmail.com>
2025-03-01 12:31:21 +0900

    Refactor: 린트에러 수정

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import React from 'react';
-import Loading from "@/src/components/loading/Loading";
-import SelectUploadImageLoading from "@/src/app/_select/_choice-image/SelectUploadImageLoading";
+// import Loading from "@/src/components/loading/Loading";
+// import SelectUploadImageLoading from "@/src/app/_select/_choice-image/SelectUploadImageLoading";
 import TemplateRequested from "@/src/app/_select/TemplateRequested";
 
 function Page() {
